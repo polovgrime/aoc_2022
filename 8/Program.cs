f49f3b189ad5dcbc49d4c540d9616013cf0b8d6a
horrest <polovgrime@gmail.com>
2023-10-30 23:50:58 +0300

    Day 8. Task 2. Finished

--- 8/Program.cs
+++ 8/Program.cs
@@ -5,7 +5,7 @@ var width = input[0].Length;
 var visibleTrees = (width + height - 2) * 2;
 
 var innerVisibleTrees = new HashSet<Tuple<int,int>>();
-
+var scenePoints = new List<int>();
 
 for (int i = 1; i < height - 1; i++)
 {
@@ -48,6 +48,17 @@ for (int i = 1; i < height - 1; i++)
 
 Console.WriteLine("Visible from inner: " + innerVisibleTrees.Count);
 Console.WriteLine("All Visible trees: " + (innerVisibleTrees.Count + visibleTrees));
+
+for (int i = 0; i < width; i++)
+{
+	for (int j = 0; j < height; j++)
+	{
+		scenePoints.Add(CountUntilBlocked(input[i][j], i, j));
+	}
+}
+
+Console.WriteLine("Highest scene point: " + scenePoints.Max());
+
 void WritePoint(int i, int j, HashSet<Tuple<int, int>> innerVisibleTrees)
 {
 
@@ -58,3 +69,55 @@ void WritePoint(int i, int j, HashSet<Tuple<int, int>> innerVisibleTrees)
 	}
 }
 
+int CountUntilBlocked(char treeHeight, int x, int y)
+{
+	var left = 0;
+
+	for (int i = x + 1; i < width; i++)
+	{
+		left++;
+
+		if (input![i][y] >= treeHeight)
+		{
+			break;
+		}
+
+	}
+
+	var right = 0;
+
+	for (int i = x - 1; i >= 0; i--)
+	{
+		right++;
+
+		if (input![i][y] >= treeHeight)
+		{
+			break;
+		}
+	}
+	
+	var top = 0;
+
+	for (int j = y + 1; j < height; j++)
+	{
+		top++;
+
+		if (input![x][j] >= treeHeight)
+		{
+			break;
+		}
+	}
+
+	var bottom = 0;
+	for (int j = y - 1; j >= 0; j--)
+	{
+		bottom++;
+
+		if (input![x][j] >= treeHeight)
+		{
+			break;
+		}
+	}
+
+	return left * right * top * bottom;
+}
